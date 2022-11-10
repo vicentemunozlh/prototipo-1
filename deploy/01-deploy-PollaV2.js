29711d22373e3032cc7b29480a87e5bde48eb019
vicentemunozlh <91915615+vicentemunozlh@users.noreply.github.com>
2022-11-10 17:54:58 -0300

    Update 01-deploy-PollaV2.js

--- deploy/01-deploy-PollaV2.js
+++ deploy/01-deploy-PollaV2.js
@@ -1,11 +1,15 @@
 const { network } = require("hardhat")
-const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { developmentChains, networkConfig, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+    
     log("----------------------------------------------------")
 
     const entranceFee = networkConfig[chainId]["entranceFee"]
