435008a0afc7eca718f0b0b5d13b2664ec3aa4ee
vicentemunozlh <91915615+vicentemunozlh@users.noreply.github.com>
2022-11-10 17:51:44 -0300

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -26,8 +26,8 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"]
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
-const frontEndContractsFile = "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
-const frontEndAbiFile = "../nextjs-smartcontract-lottery-fcc/constants/abi.json"
+const frontEndContractsFile = "../nextjs-smartcontract-polla-fcc/constants/contractAddresses.json"
+const frontEndAbiFile = "../nextjs-smartcontract-polla-fcc/constants/abi.json"
 
 module.exports = {
     networkConfig,
